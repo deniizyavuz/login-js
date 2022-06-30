4d15e728f8748f43fbc86fd114add2918e2e5961
denizyavuz99 <denizyavuz.99@gmail.com>
2022-06-30 23:44:49 +0300

    paketler eklendi

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,57 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    app: './src/index.js',
+    EmailsInput: './src/components/emails-input/emails-input.js',
   },
-  mode: 'development'
+  output: {
+    filename: '[name].dist.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: '[name]',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+  },
+  mode: 'development',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].dist.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // eslint-disable-next-line global-require
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
