12388f9fc4b30328193dd56a3247606da3cf9a52
denizyavuz99 <denizyavuz.99@gmail.com>
2022-07-07 22:39:16 +0300

    distler ignore edildi

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-/* eslint-disable linebreak-style */
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.use("/dist", express.static(path.join(__dirname, "dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 app.listen(process.env.PORT || 4000);
